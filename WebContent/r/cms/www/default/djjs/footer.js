a55888603fe3780af55003816e93f0497095f7c8
zhaohuaan <zhaohuaan0925@163.com>
2016-05-30 16:04:06 +0800

    web端全部完成

--- WebContent/r/cms/www/default/djjs/footer.js
+++ WebContent/r/cms/www/default/djjs/footer.js
@@ -4,8 +4,8 @@ document.writeln('<div class="footer">'+
 					'<div class="div_mid">'+
 						'<ul class="over_flow">'+
 							'<li>CPC&nbsp;Jiuxianqiao&nbsp;Street&nbsp;Party&nbsp;Party&nbsp;Organization&nbsp;Information&nbsp;Integrated&nbsp;Platform</li>'+
-							'<li>酒仙桥街道党员党组织信息综合平台&nbsp;版权所有◎2016-2019&nbsp;京ICP备13010694号</li>'+
-							'<li>技术支持：酒仙桥&nbsp;联系电话&nbsp;：010-2652594&nbsp;QQ：154637585</li>'+
+							'<li>酒仙桥街道党员党组织综合信息平台&nbsp;版权所有◎2016-2066&nbsp;京ICP备13010694号</li>'+
+							'<li>技术支持：博研正通（北京）信息咨询有限公司&nbsp;联系电话&nbsp;：010-58347330&nbsp;QQ：2394697151</li>'+
 							'<li><a target="_blank" href="/about.jhtml">关于我们</a>&nbsp;&nbsp;<a onclick="SetHome(this,window.location)" href="javascript:void(0)">设为首页</a>&nbsp;&nbsp;<a onclick="AddFavorite(window.location,document.title)" href="javascript:void(0)">加入收藏</a></li>'+
 							'<li>pv总量：<span id="pv"></span>&nbsp;&nbsp;访客数总量：<span id="visitor"></span>&nbsp;&nbsp;今日pv：<span id="dayPv"></span>&nbsp;&nbsp;今日访客数：<span id="dayVisitor"></span></li>'+
 						'</ul>'+
